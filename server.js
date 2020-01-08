32944ec477cc0202374e8a310db907ea5e8d479b
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2020-01-08 18:06:03 -0300

    limpeza em server.js e ProductController.js

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir')
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(
-  'mongodb://localhost:27017/nodeapi', 
+  'mongodb://localhost:27017/node-api', 
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+app.post('/api/teste', (req, res) => {
+  const product = Product.create({
+    title: 'Titulo5',
+    description: 'Decricao da Outra Outra Coisa',
+    url: 'http://www.testao.com'
+  });
+  return res.send('Ola Mundo');
+});
+
 
 app.use('/api', require('./src/routes'));
 app.listen(3001);
